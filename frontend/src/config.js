f7a041a0020066b2a27134d4bfc543b410ea4f46
Umang <umg2508@gmail.com>
2025-05-25 17:28:02 +0530

    static backend url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,2 @@
-export const BACKEND_SOCKET_URL = import.meta.env.BACKEND_SOCKET_URL;
-//export const BACKEND_SOCKET_URL = "https://personal-ai-chatbot.onrender.com";
-
+export const BACKEND_SOCKET_URL = "https://personal-ai-chatbot.onrender.com";
 
