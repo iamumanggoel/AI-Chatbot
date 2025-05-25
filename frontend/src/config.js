a6de2cdd775a00368c7186d635861f6ee65702db
Umang <umg2508@gmail.com>
2025-05-25 17:17:25 +0530

    backend url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const BACKEND_SOCKET_URL = import.meta.env.VITE_BACKEND_SOCKET_URL;
-//export const BACKEND_SOCKET_URL = "https://personal-chatbot-rbyd.onrender.com";
+export const BACKEND_SOCKET_URL = import.meta.env.BACKEND_SOCKET_URL;
+//export const BACKEND_SOCKET_URL = "https://personal-ai-chatbot.onrender.com";
 
 
